f0d140e9a6b037d729bf7644797176f05dd16a42
Looobay <69517054+Looobay@users.noreply.github.com>
2024-09-08 22:06:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,33 @@
-const {
-  useState
-} = React;
-function Button({
-  text,
-  onClick
-}) {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
-    onClick: onClick
-  }, text));
-}
-function FunButton() {
-  let [number, setNumber] = useState(0);
-  function FunClick() {
-    setNumber(number + 1);
-  }
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, number), /*#__PURE__*/React.createElement("button", {
-    onClick: FunClick
-  }, "Funny lol"));
-}
 function App() {
-  function classic() {
-    alert("Hello React!");
-  }
-  function counting() {
-    setCount(count + 1);
-  }
   return /*#__PURE__*/React.createElement("div", {
     className: "App"
   }, /*#__PURE__*/React.createElement("header", {
     className: "App-header"
-  }, /*#__PURE__*/React.createElement("title", null, "React in HTML"), /*#__PURE__*/React.createElement("link", {
+  }, /*#__PURE__*/React.createElement("title", null, "Looobay \uD83D\uDE03"), /*#__PURE__*/React.createElement("link", {
     rel: "stylesheet",
     href: "styles.css"
-  }), /*#__PURE__*/React.createElement("img", {
-    src: "./img/react.png",
-    className: "App-logo"
-  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
-    className: "App-link",
-    href: "https://github.com/Looobay/react-in-html",
-    target: "_blank",
-    rel: "noopener noreferrer"
-  }, "Github Page"), /*#__PURE__*/React.createElement("a", {
-    className: "App-link",
-    href: "https://react.dev/learn",
-    target: "_blank",
-    rel: "noopener noreferrer"
-  }, "Learn React"), /*#__PURE__*/React.createElement(Button, {
-    className: "App-button",
-    text: 'Classic React Button',
-    onClick: classic
-  }), /*#__PURE__*/React.createElement(FunButton, null)));
+  }), /*#__PURE__*/React.createElement("h1", null, "Welcome on my Github Page!"), /*#__PURE__*/React.createElement("a", {
+    href: "react/index.html",
+    className: "App-link"
+  }, "Click here to see a clean small React demo"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", null, "Looobay Public License"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL2_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-2 (LPL-2) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL2_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-2 (LPL-2) in French."), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1-1_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1.1 (LPL-1.1) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1-1_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1.1 (LPL-1.1) in French."), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1 (LPL-1) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1 (LPL-1) in French.")));
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(App());
