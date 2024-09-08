77bff26abfdc14ba023513dca399f4b75ffb78ec
Looobay <69517054+Looobay@users.noreply.github.com>
2024-09-08 22:08:46 +0200

    pls

---
+++ app.js
@@ -0,0 +1,33 @@
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  }, /*#__PURE__*/React.createElement("header", {
+    className: "App-header"
+  }, /*#__PURE__*/React.createElement("title", null, "Looobay \uD83D\uDE03"), /*#__PURE__*/React.createElement("link", {
+    rel: "stylesheet",
+    href: "styles.css"
+  }), /*#__PURE__*/React.createElement("h1", null, "Welcome on my Github Page!"), /*#__PURE__*/React.createElement("a", {
+    href: "react/index.html",
+    className: "App-link"
+  }, "Click here to see a clean small React demo"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("h2", null, "Looobay Public License"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL2_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-2 (LPL-2) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL2_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-2 (LPL-2) in French."), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1-1_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1.1 (LPL-1.1) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1-1_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1.1 (LPL-1.1) in French."), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1_EN.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1 (LPL-1) in English."), /*#__PURE__*/React.createElement("a", {
+    href: "license/LPL1_FR.txt",
+    className: "App-link"
+  }, "Click here to read the Looobay Public License-1 (LPL-1) in French.")));
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(App());
